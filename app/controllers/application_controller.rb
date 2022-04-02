2a55ab8201f261a2c18131dd065598119db9591a
Gustavo62 <gust904@gmail.com>
2022-04-01 23:34:37 -0300

    adaptando layout para real estrutura com variáveis, adcionando avatar attach ao user, e alterando layout de todas as telas do crud de user-devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:password,:nome,:bio,:link_linkedin,:avatar])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:email,:password])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email,:password,:account_update,:nome,:bio,:link_linkedin,:avatar])
+
+  end
 end
